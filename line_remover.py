bf9181718de4a7e25b432bc6776d31a6eee8cd03
nicolasdarmanthe <nicolasdarmanthe@users.noreply.github.com>
2020-08-16 17:16:14 +1000

    Original upload

---
+++ line_remover.py
@@ -0,0 +1,13 @@
+import keyboard
+import pyperclip
+import time
+    
+while True:
+    if keyboard.is_pressed('ctrl+c'):
+        time.sleep(0.1)
+        texttomod = pyperclip.paste()
+        modtext = texttomod.replace('\r\n','')
+        pyperclip.copy(modtext)
+        
+        
+        
